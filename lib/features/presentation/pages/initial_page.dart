28880125317de193a5b44d3f7de7075db5b17568
Shruti41 <sgarasangi11@gmail.com>
2025-08-04 18:02:38 +0530

    Add contact UI feature

--- lib/features/presentation/pages/initial_page.dart
+++ lib/features/presentation/pages/initial_page.dart
@@ -1,18 +1,16 @@
 import 'dart:io';
 
 import 'package:device_info_plus/device_info_plus.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:permission_handler/permission_handler.dart';
+//import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:truecaller_clone/core/l10/app_localizations.dart';
 import 'package:truecaller_clone/features/presentation/pages/language_screen.dart';
-import 'package:truecaller_clone/features/presentation/widgets/custom_bottom_navigation.dart';
 import 'package:truecaller_clone/platform_channel.dart';
 
 import '../utilities/colors.dart';
 import '../widgets/custom_button.dart';
 import '../widgets/permission_popup.dart';
-import 'backup_screen.dart';
 
 class InitialPage extends StatefulWidget {
   const InitialPage({super.key});
@@ -33,91 +31,126 @@ class _InitialPageState extends State<InitialPage> {
               // mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Text(AppLocalizations.of(context)!.safeDial,style: Theme.of(context).textTheme.displaySmall?.copyWith(color: Colors.blue,fontWeight: FontWeight.bold),),
-                const SizedBox(height: 20,),
-                Text(AppLocalizations.of(context)!.knowText,style: Theme.of(context).textTheme.headlineLarge?.copyWith(color: Colors.black,fontWeight: FontWeight.bold),),
-                const SizedBox(height: 30,),
+                Text(
+                  AppLocalizations.of(context)!.safeDial,
+                  style: Theme.of(context).textTheme.displaySmall?.copyWith(
+                      color: Colors.blue, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  AppLocalizations.of(context)!.knowText,
+                  style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                      color: Colors.black, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Container(
                       height: 10,
                       width: 40,
-
                       decoration: const BoxDecoration(
                           color: Colors.blue,
-                        borderRadius: BorderRadius.all(Radius.circular(10))
-                      ),
+                          borderRadius: BorderRadius.all(Radius.circular(10))),
                     ),
-                    const SizedBox(width: 10,),
+                    const SizedBox(
+                      width: 10,
+                    ),
                     Container(
                       height: 10,
                       width: 40,
-
                       decoration: const BoxDecoration(
                           color: Colors.grey,
-                          borderRadius: BorderRadius.all(Radius.circular(10))
-                      ),
+                          borderRadius: BorderRadius.all(Radius.circular(10))),
                     ),
-                    const SizedBox(width: 10,),
+                    const SizedBox(
+                      width: 10,
+                    ),
                     Container(
                       height: 10,
                       width: 40,
-
                       decoration: const BoxDecoration(
                           color: Colors.grey,
-                          borderRadius: BorderRadius.all(Radius.circular(10))
-                      ),
+                          borderRadius: BorderRadius.all(Radius.circular(10))),
                     ),
                   ],
                 ),
-                const Placeholder(fallbackHeight: 200,fallbackWidth: 10,),
-                const SizedBox(height: 50,),
+                const Placeholder(
+                  fallbackHeight: 200,
+                  fallbackWidth: 10,
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
                 GestureDetector(
-                  onTap: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (_)=>LanguageScreen()));
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (_) => LanguageScreen()));
                   },
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Icon(Icons.language,color: Colors.blue.shade900,size: 30,),
-                      Text(AppLocalizations.of(context)!.changeLanguage,style: Theme.of(context).textTheme.headlineSmall?.copyWith(color: Colors.blue.shade900,fontWeight: FontWeight.normal),),
+                      Icon(
+                        Icons.language,
+                        color: Colors.blue.shade900,
+                        size: 30,
+                      ),
+                      Text(
+                        AppLocalizations.of(context)!.changeLanguage,
+                        style: Theme.of(context)
+                            .textTheme
+                            .headlineSmall
+                            ?.copyWith(
+                                color: Colors.blue.shade900,
+                                fontWeight: FontWeight.normal),
+                      ),
                     ],
                   ),
                 ),
-                const SizedBox(height: 30,),
-
+                const SizedBox(
+                  height: 30,
+                ),
                 customButton(
-                    onPressed: ()  async {
+                    onPressed: () async {
                       await _requestPermissions();
                       WidgetsBinding.instance.addPostFrameCallback((_) {
                         CallPlatformChannel.startCallService();
                         CallPlatformChannel.requestDialerRole();
                       });
-                      Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (_)=>PermissionPopupStep1()), (Route<dynamic> route)=> false );
+                      Navigator.pushAndRemoveUntil(
+                          context,
+                          MaterialPageRoute(
+                              builder: (_) => PermissionPopupStep1()),
+                          (Route<dynamic> route) => false);
                     },
                     label: AppLocalizations.of(context)!.getStarted,
                     width: MediaQuery.sizeOf(context).width,
                     height: 60,
                     isLoading: false,
-                    backgroundColor:CustomColors.buttonColor,
+                    backgroundColor: CustomColors.buttonColor,
                     //Colors.green,
                     borderRadius: 16,
                     textColor: Colors.white,
                     fontSize: 20
                     // border: Border.all(color: Colors.deepPurple)
+                    ),
+                const SizedBox(
+                  height: 16,
                 ),
-                const SizedBox(height: 16,),
-                Text(AppLocalizations.of(context)!.privacyText,style: Theme.of(context).textTheme.titleLarge?.copyWith(color: Colors.grey,fontWeight: FontWeight.normal),),
-
-
-
+                Text(
+                  AppLocalizations.of(context)!.privacyText,
+                  style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                      color: Colors.grey, fontWeight: FontWeight.normal),
+                ),
               ],
             ),
           ),
         ),
       ),
-
     );
   }
 
@@ -125,13 +158,14 @@ class _InitialPageState extends State<InitialPage> {
     final statuses = await [
       Permission.phone,
       Permission.contacts,
-
       Permission.systemAlertWindow,
       Permission.notification,
-      if (Platform.isAndroid && await DeviceInfoPlugin().androidInfo.then((info) => info.version.sdkInt >= 34))
+      if (Platform.isAndroid &&
+          await DeviceInfoPlugin()
+              .androidInfo
+              .then((info) => info.version.sdkInt >= 34))
         Permission.ignoreBatteryOptimizations,
     ].request();
-
 
     if (statuses.values.any((status) => status.isPermanentlyDenied)) {
       await openAppSettings();
--- lib/features/presentation/pages/language_screen.dart
+++ lib/features/presentation/pages/language_screen.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:truecaller_clone/core/l10/app_localizations.dart';
+//import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:truecaller_clone/features/presentation/pages/initial_page.dart';
 
 import '../../../main.dart';
@@ -14,37 +14,110 @@ class LanguageScreen extends StatefulWidget {
 
 class _LanguageScreenState extends State<LanguageScreen> {
   final List<Map<String, dynamic>> languages = [
-    {'code': 'hi', 'label': 'हिंदी', 'symbol': 'ह', 'color': Colors.orangeAccent},
-    {'code': 'en', 'label': 'English', 'symbol': 'E', 'color': Colors.blue.shade300},
-    {'code': 'bn', 'label': 'বাংলা', 'symbol': 'বা', 'color': Colors.teal.shade200},
-    {'code': 'te', 'label': 'తెలుగు', 'symbol': 'తె', 'color': Colors.red.shade200},
-    {'code': 'mr', 'label': 'मराठी', 'symbol': 'म', 'color': Colors.indigo.shade200},
-    {'code': 'ta', 'label': 'தமிழ்', 'symbol': 'த', 'color': Colors.cyan.shade300},
-    {'code': 'kn', 'label': 'ಕನ್ನಡ', 'symbol': 'ಕ', 'color': Colors.green.shade300},
-    {'code': 'gu', 'label': 'ગુજરાતી', 'symbol': 'ગુ', 'color': Colors.purple.shade200},
-    {'code': 'pa', 'label': 'ਪੰਜਾਬੀ', 'symbol': 'ਪੰ', 'color': Colors.blueAccent.shade100},
-    {'code': 'ml', 'label': 'മലയാളം', 'symbol': 'മ', 'color': Colors.lightGreen.shade200},
-    {'code': 'ur', 'label': 'اردو', 'symbol': 'ا', 'color': Colors.orange.shade100},
+    {
+      'code': 'hi',
+      'label': 'हिंदी',
+      'symbol': 'ह',
+      'color': Colors.orangeAccent
+    },
+    {
+      'code': 'en',
+      'label': 'English',
+      'symbol': 'E',
+      'color': Colors.blue.shade300
+    },
+    {
+      'code': 'bn',
+      'label': 'বাংলা',
+      'symbol': 'বা',
+      'color': Colors.teal.shade200
+    },
+    {
+      'code': 'te',
+      'label': 'తెలుగు',
+      'symbol': 'తె',
+      'color': Colors.red.shade200
+    },
+    {
+      'code': 'mr',
+      'label': 'मराठी',
+      'symbol': 'म',
+      'color': Colors.indigo.shade200
+    },
+    {
+      'code': 'ta',
+      'label': 'தமிழ்',
+      'symbol': 'த',
+      'color': Colors.cyan.shade300
+    },
+    {
+      'code': 'kn',
+      'label': 'ಕನ್ನಡ',
+      'symbol': 'ಕ',
+      'color': Colors.green.shade300
+    },
+    {
+      'code': 'gu',
+      'label': 'ગુજરાતી',
+      'symbol': 'ગુ',
+      'color': Colors.purple.shade200
+    },
+    {
+      'code': 'pa',
+      'label': 'ਪੰਜਾਬੀ',
+      'symbol': 'ਪੰ',
+      'color': Colors.blueAccent.shade100
+    },
+    {
+      'code': 'ml',
+      'label': 'മലയാളം',
+      'symbol': 'മ',
+      'color': Colors.lightGreen.shade200
+    },
+    {
+      'code': 'ur',
+      'label': 'اردو',
+      'symbol': 'ا',
+      'color': Colors.orange.shade100
+    },
   ];
-
-
 
   @override
   Widget build(BuildContext context) {
-    final localization=AppLocalizations.of(context)!;
-    return  Scaffold(
+    final localization = AppLocalizations.of(context)!;
+    return Scaffold(
       body: Center(
         child: SafeArea(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             // mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(AppLocalizations.of(context)!.safeDial,style: Theme.of(context).textTheme.displaySmall?.copyWith(color: Colors.blue,fontWeight: FontWeight.bold),),
-              SizedBox(height: 20,),
-              Text(AppLocalizations.of(context)!.welcomeText,style: Theme.of(context).textTheme.displaySmall?.copyWith(color: Colors.black,fontWeight: FontWeight.normal),),
-              SizedBox(height: 20,),
-              Text(AppLocalizations.of(context)!.pickYourLang,style: Theme.of(context).textTheme.headlineSmall?.copyWith(color: Colors.grey,fontWeight: FontWeight.normal),),
-              SizedBox(height: 20,),
+              Text(
+                AppLocalizations.of(context)!.safeDial,
+                style: Theme.of(context)
+                    .textTheme
+                    .displaySmall
+                    ?.copyWith(color: Colors.blue, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                AppLocalizations.of(context)!.welcomeText,
+                style: Theme.of(context).textTheme.displaySmall?.copyWith(
+                    color: Colors.black, fontWeight: FontWeight.normal),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                AppLocalizations.of(context)!.pickYourLang,
+                style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                    color: Colors.grey, fontWeight: FontWeight.normal),
+              ),
+              SizedBox(
+                height: 20,
+              ),
               // Row(
               //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               //   children: [
@@ -82,11 +155,13 @@ class _LanguageScreenState extends State<LanguageScreen> {
       ),
     );
   }
+
   Widget _languageCard(String symbol, String name, String code, Color color) {
     return InkWell(
       onTap: () {
         MyApp.setLocale(context, Locale(code));
-        Navigator.push(context, MaterialPageRoute(builder: (_)=>InitialPage()));
+        Navigator.push(
+            context, MaterialPageRoute(builder: (_) => InitialPage()));
       },
       child: Container(
         padding: const EdgeInsets.all(12),
@@ -107,7 +182,8 @@ class _LanguageScreenState extends State<LanguageScreen> {
               alignment: Alignment.center,
               child: Text(
                 symbol,
-                style:  TextStyle(fontSize: 42, fontWeight: FontWeight.bold,color: color),
+                style: TextStyle(
+                    fontSize: 42, fontWeight: FontWeight.bold, color: color),
               ),
             ),
             const SizedBox(width: 10),
@@ -123,6 +199,4 @@ class _LanguageScreenState extends State<LanguageScreen> {
       ),
     );
   }
-
-
 }
